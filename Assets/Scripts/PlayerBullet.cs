e3ecc197782a3554f586095798b193744ef1e409
crassSandwich <christopherjanidlo@gmail.com>
2019-10-11 20:31:32 -0400

    Targeting and shooting

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class PlayerBullet : MonoBehaviour
+{
+    public float Speed, Damage;
+
+    public void Initialize (Vector2 direction)
+    {
+        transform.up = direction;
+        GetComponent<Rigidbody2D>().velocity = direction * Speed;
+    }
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        try
+        {
+            other.gameObject.GetComponent<EnemyHealth>().Damage(Damage);
+        }
+        finally
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnBecameInvisible ()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public PlayerBullet BulletPrefab;
+    public float BulletsPerSecond, MaxFireRange;
+
+    float timePerBullet => 1.0f / BulletsPerSecond;
+
+    float fireTimer;
+
+    void Update ()
+    {
+        var target = acquireTarget();
+        
+        if (target != null)
+        {
+            Vector2 dir = (target.transform.position - transform.position).normalized;
+            transform.GetChild(0).up = dir;
+
+            if (fireTimer <= 0)
+            {
+                fireTimer = timePerBullet;
+                Instantiate(BulletPrefab, transform.position, Quaternion.identity).Initialize(dir);
+            }
+        }
+
+        fireTimer -= Time.deltaTime;
+    }
+
+    EnemyHealth acquireTarget () => FindObjectsOfType<EnemyHealth>()?.
+             Select(eh => new { eh, distance = Vector2.Distance(transform.position, eh.transform.position) })
+            .Where(e => e.distance <= MaxFireRange)
+            .OrderBy(e => e.distance)
+            .FirstOrDefault().eh;
+}
