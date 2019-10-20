097c25c60ad9948174f1c2b89e32d7c3dfd77b41
crassSandwich <christopherjanidlo@gmail.com>
2019-10-20 10:48:10 -0400

    Background changes

---
+++ Assets/Scripts/RandomRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using crass;
+
+public class RandomRotate : MonoBehaviour
+{
+    private static BagRandomizer<float> rotations = new BagRandomizer<float>
+    {
+        Items = new List<float> { 0, 90, 180, 270 },
+        AvoidRepeats = true
+    };
+
+    void Start ()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, rotations.GetNext());
+    }
+}
